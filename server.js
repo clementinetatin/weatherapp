812df674318efb096c86d9699b0cade2b78f95e8
clementinetatin <64962470+clementinetatin@users.noreply.github.com>
2020-06-15 09:12:56 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ app.use(express.static('website'));
 const port = 3000;
 const server = app.listen(port,listening);
 
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
 /*app.get('/',function (req,res) {
   res.send('hello world');
 });*/
--- website/app.js
+++ website/app.js
@@ -1,6 +1,6 @@
 /* Global Variables */
-let baseURL = 'api.openweathermap.org/data/2.5/weather?zipCode=';
-let apiKey = '9a04e7a51c4c5dead4da8a1213fe268b';
+let baseURL = 'api.openweathermap.org/data/2.5/weather?q=';
+let apiKey = 'c54018a8d4ca926d38b29c05e6e7e797';
 const zipCode = document.getElementById('zip').value;
 
 
@@ -29,7 +29,7 @@ function performAction(e) {
 
 // GET the infos from the openweathermap API
 const getWeather = async (baseURL, zipCode, apiKey) => {
-  const res = await fetch(baseURL + zipCode + apiKey); // avant : ,
+  const res = await fetch(baseURL+zipCode+apiKey); // avant : ,
   try {
     const data = await res.json();
     console.log(data)
@@ -38,7 +38,7 @@ const getWeather = async (baseURL, zipCode, apiKey) => {
     console.log("error in Get API infos", error);
   }
 }
-// ASYNC POST function to get the datas from our server ==> à voir si ne pas mettre à la fin
+// ASYNC POST function to get the datas from our server
 const postData = async (url='', data = {}) => {
   console.log(data);
   const response = await fetch(url, { //'http://localhost:3000/add'
@@ -47,7 +47,11 @@ const postData = async (url='', data = {}) => {
     headers : {
       'Content-Type':'application/json',
     },
-    body : JSON.stringify(data),
+    body : JSON.stringify({
+      date : data.date,
+      temp : data.temp,
+      content : data.content
+    })
   });
   try {
     const newData = await response.json();
@@ -65,11 +69,11 @@ const updateUI = async() => {
   try {
     const allData = await request.json()
     console.log(allData)
-    // var index = Object.keys(allData).length -1;
-    document.getElementById('date').innerHTML = 'Today is '+ allData[0].date; // dans ce cas remplacer 0 par index
-    document.getElementById('temp').innerHTML = 'The temperatrure is ' + allData[0].temp;
-    document.getElementById('content').innerHTML = 'And I feel ' + allData[0].content;
+    // update infos
+    document.getElementById('date').innerHTML = 'Today is '+ allData.date; // dans ce cas remplacer 0 par index
+    document.getElementById('temp').innerHTML = 'The temperatrure is ' + allData.temp;
+    document.getElementById('content').innerHTML = 'And I feel ' + allData.content;
   } catch(error) {
     console.log("error in update UI", error);
   }
-}
+};
